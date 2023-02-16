7fa54066cdc40d367dced7ed722ea47623dddfce
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-16 22:58:13 +0100

    fix:add script C programs rpint with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - main c function
+ *Description: Function contains a printout of a message
+ *Return:0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
