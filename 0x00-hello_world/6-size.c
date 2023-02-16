7429e930abf3a056fbab045d2bab0111b8630e11
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-16 23:18:44 +0100

    add script print data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry to function
+ *Description: prints various size of types
+ *Return: 0
+ */
+
+int main(void)
+{
+
+    printf("Size of a char: %zu byte(s)\n", sizeof(char));
+    printf("Size of an int: %zu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %zu byte(s)\n", sizeof(float));
+    return (0);
+
+}
