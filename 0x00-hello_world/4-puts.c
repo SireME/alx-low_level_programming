fc1d18d9ec0aa68e8996de30394d61d7b9d4c20e
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-16 22:48:08 +0100

    fix:add script C programs that prints using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main - main c function
+ *Description: The main function for the c code
+ *Return:0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
