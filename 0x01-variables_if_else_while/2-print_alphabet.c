d3be6ad9f03906bc57ceac2f031c3031fd81fa06
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-20 19:20:22 +0100

    add script print lowercase alphabet characters followed by newline

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - ini function
+ * Description: The entry into the script
+ * Return:0
+ **/
+
+
+int main(void)
+{
+	char *nl = "\n";
+
+	for (int x = 'a'; x <= 'z'; x++)
+	{
+	putchar(x);
+	}
+	putchar (*nl);
+}
+
