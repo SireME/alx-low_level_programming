43182977fc83e1c48e50bdfe6c209f5ba2101fa3
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-21 14:23:19 +0100

    add script print underscore letters in another function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * print_alphabet - rpint alphabets
+ * Description: Prints our alphabets from a to z in lower case
+ * Return: returnns nothing
+ **/
+
+
+void print_alphabet(void)
+{
+	char *nl = "\n";
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar (*nl);
+
+	return;
+}
+
+/**
+ * main - ini function
+ * Description: print script followed by new line
+ * Return:0
+ **/
+
+int main(void)
+{
+print_alphabet();
+	return (0);
+}
