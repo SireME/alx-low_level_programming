bead169c3803e911e68a81cc13d0548c4c69176c
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-22 10:16:11 +0100

    add script vompute absolute avlue of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _abs - absoluet value of int
+ * @n: value to be computed
+ * Description: computes the absolute value of an integer
+ * Return: integer
+ **/
+
+int _abs(int n)
+
+{
+	int vl;
+
+	vl = abs(n);
+	return (vl);
+}
+
