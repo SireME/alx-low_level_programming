c1294dc874c9dccb0106243af6fd6dd746ef0316
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-22 13:26:16 +0100

    add script add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * add - add two integers
+ * @c: first int
+ * @n: second integer
+ * Description: program that sums two integers
+ * Return: sum of numbers
+ **/
+
+int add(int n, int c)
+
+{
+	return (n+c);
+}
+
