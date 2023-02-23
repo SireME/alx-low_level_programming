75c95c6dc3332e6178263b7b73492a0e0661fccc
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-23 22:48:13 +0100

    print lines from integer input

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_line - code to draw a line using number specifiers
+ * @n: number of line to be printed
+ * Description: the following function draws a line from input
+ * Return: nada
+ **/
+
+void print_line(int n)
+
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int vl;
+
+		for (vl = 1; vl <= n; vl++)
+		{
+			_putchar('_');
+		}
+	}
+}
+
