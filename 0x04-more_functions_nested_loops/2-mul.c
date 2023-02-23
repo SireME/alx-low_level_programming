df50e898f9ecb4285b98ab7a908c7ebe953aaf2d
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-23 17:33:59 +0100

    multiply and return the product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * mul - code to multiply two numbers
+ * @a: first integer to be multplied
+ * @b: second integer to be multiplied
+ * Description: the following function multplies and retuns wto numbers
+ * Return: binary 1 or 0
+ **/
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
+
+
