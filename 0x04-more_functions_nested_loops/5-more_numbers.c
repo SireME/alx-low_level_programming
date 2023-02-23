f023f9dd2bb22f7758c600184e60d5c3e236f1fd
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-23 20:47:55 +0100

    print numbers from 0 to 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - function to print numbers
+ * Description: function that prints numbers from 0 to 14 10 times
+ * Return: nada
+ **/
+
+void print_numbers(void)
+
+{
+	int externl;
+
+	for (externl = 0; externl < 10; externl++)
+	{
+		int vl;
+
+		for (vl = 0; vl < 15; vl++)
+		{
+			if (vl < 10)
+			{
+				_putchar('0' + vl);
+			}
+			if (vl > 9)
+			{
+				_putchar((vl / 10) + '0');
+				_putchar((vl % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
