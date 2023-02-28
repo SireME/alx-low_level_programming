f9dd0dd2353bd566789c8834ffa84b6f476f5295
Mbang Ernest N <mbangernest@yahoo.com>
2023-02-28 09:42:19 +0100

    convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,5 +11,20 @@
 
 int _atoi(char *s)
 {
-	return (atoi(s));
+	int sign = 1, i = 0;
+	unsigned int fl = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		fl = (fl * 10) + (s[i] - '0');
+		i++;
+	}
+	fl *= sign;
+	return (fl);
 }
