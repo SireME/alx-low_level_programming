50db7ccad4728ad49620e57a314fea40232e841a
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-01 16:02:08 +0100

    compare two strings to determine if same

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+/**
+ * _strcmp - compare strings
+ * @s1: string to be compared
+ * @s2: second string to be compared
+ * Description: function that compares two strings and determine if same
+ * Return: nada
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
+
