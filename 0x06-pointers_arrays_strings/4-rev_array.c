19daa0ecbe2bebb8f8820ac01bbe1a5868315c05
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-01 18:16:33 +0100

    reverse values of an integer arrya

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+/**
+ * reverse_array - print strings in reverse
+ * @a: integer array to be reversed
+ * @n: number of integer elements
+ * Description: function that reverses an array of inits
+ * Return: nada
+ **/
+
+void reverse_array(int *a, int n)
+{
+	int tmp[n - 1];
+
+	for (int i = 0; i < n; i++)
+	{
+		tmp[i] = a[n - 1 - i];
+	}
+	for (int j = 0; j < n; j++)
+	{
+		a[j] = tmp[j];
+	}
+}
