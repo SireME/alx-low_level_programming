360ce13a9af94570ffe9a08d081a4f3ae2b385be
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-01 22:19:15 +0100

    reverse integer string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,13 +11,13 @@
 
 void reverse_array(int *a, int n)
 {
-	int tmp[n - 1];
+	int tmp[n - 1], i, j;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		tmp[i] = a[n - 1 - i];
 	}
-	for (int j = 0; j < n; j++)
+	for (j = 0; j < n; j++)
 	{
 		a[j] = tmp[j];
 	}
