fa85437a217ab0bb4dcaa795fa317851794f64fa
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-02 21:28:31 +0100

    encode string using rod13 algorithm

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,6 +15,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *n);
 char *cap_string(char *s);
 char *leet(char *s);
+char *rot13(char *s);
 int _putchar(char c);
 int _abs(int);
 int _isupper(int c);
