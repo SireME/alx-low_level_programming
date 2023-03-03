60734a65d12d8900de70cd4adc2104396fda8404
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-03 03:28:25 +0100

    print integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,52 +1,24 @@
 #include "main.h"
 
 /**
- * base10 - power in 10 base
- * @n: an exponent
- * Return: returns 10 to power exponent
+ * print_number - print integer entered.
+ * @n: The integer to be printed.
+ * Return: nothing
  */
-int base10(int n)
-{
-	int base = 10;
 
-	while (n > 0)
-	{
-		base *= 10;
-		n--;
-	}
-	return (base);
-}
-
-/**
- * print_number - print integer entered
- * @n: integer to print
- * Return: void
- */
 void print_number(int n)
 {
-	int power;
-
-	power = base10(8);
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num = -num;
 	}
 
-	if (n == 0)
-		_putchar('0');
-
-	else
+	if ((num / 10) > 0)
 	{
-		while (n / power == 0)
-			power /= 10;
-
-		while (power >= 1)
-		{
-			_putchar((n / power) + '0');
-			n %= power;
-			power /= 10;
-		}
+		print_number(num / 10);
+		_putchar((num % 10) + '0');
 	}
 }
