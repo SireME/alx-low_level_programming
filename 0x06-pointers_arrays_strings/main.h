cbcee96e15e9845229674712953325f037de352e
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-03 03:46:52 +0100

    add two numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,6 +17,7 @@ char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 int _putchar(char c);
 int _abs(int);
 int _isupper(int c);
