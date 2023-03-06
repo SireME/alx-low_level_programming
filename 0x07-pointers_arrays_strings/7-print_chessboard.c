47874361a1ea2cede52c611e653d44dccfa49c76
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-06 19:19:00 +0100

    print chessboard (2d array)

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_chessboard -  print cheeseboard.
+ *@a: string 2d array to print.
+ * nada
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+	}
+}
