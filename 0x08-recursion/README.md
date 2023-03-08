5d719f81cb273eea8a42d9ebc868f78b489d2981
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-08 12:08:34 +0100

    create recursion dir, add README.md and main.h

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+# About  
+This directory contains information regarding recursions :0x08-recursion.
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing declartions for all function
+ */
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _putchar(char c);
+int _abs(int);
+int _isupper(int c);
+int _isdigit(int c);
+
+
+#endif
+
