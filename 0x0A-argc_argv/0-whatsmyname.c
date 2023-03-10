5652758a82210b055101acbf3d5689c3e1af29dd
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-10 22:46:25 +0100

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  main function to return standard C inputs.
+ *@argc: size of input array.
+ *@argv: array of input parameters
+ * Return: nada
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
