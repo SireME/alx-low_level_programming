11867774a72ae890d0ee39d746ca4283bf15ccee
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-10 22:33:43 +0100

    create arguments directory and create initial files

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing declartions for all function
+ */
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+
+#endif
