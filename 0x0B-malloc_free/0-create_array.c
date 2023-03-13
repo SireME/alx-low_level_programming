7558bea481b8a0779c210d15e9b22dba57a36ba9
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-13 19:08:45 +0100

    create array of char, initialize with char and return pointer to it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array -  create array and initialize with charcater.
+ *@size: size of input array.
+ *@c: charcter to initialize array.
+ * Return: pointer to array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arc;
+	int i, j = size;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arc = malloc(sizeof(char)*size);
+	for (i = 0;i < j; i++)
+	{
+		arc[i] = c;
+	}
+	return (arc);
+}
