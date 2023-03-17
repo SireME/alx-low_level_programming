080a4cb2cd4bc1e10d5986c08313813d7bca7067
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-17 13:22:58 +0100

    reallocate memory block using malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,6 +3,7 @@
 
 /**
  * _realloc -  reallocate memory.
+ * @ptr: old memory block to reallocate.
  *@old_size: size of pointer to reallocate.
  *@new_size: new size of pointer to be reallocated to.
  * Return: new reallocated pointer.
@@ -31,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	for (i = 0; i < old_size; i++) /*initialise memory with ptr values*/
-		*((char *)(nmem) + i) = *((char *)(ptr)+i);
+		*((char *)(nmem) + i) = *((char *)(ptr) + i);
 
 	free(ptr);
 	return (nmem);
