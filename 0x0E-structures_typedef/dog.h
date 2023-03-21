92f26d8a52a700c20a1bfb6f4187cfb5ec6b8a09
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-21 02:19:31 +0100

    fix: dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,10 +14,6 @@ struct dog
 	float age;
 	char *owner;
 };
-/**
- *prototypes to use in our dog functions
- *
- */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
