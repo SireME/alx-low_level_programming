29f0f65c9c5cebd2309cd89fdbb69e6ed62f79bb
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-21 02:26:16 +0100

    fix: print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,19 +13,10 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if ((*d).name == NULL)
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", (*d).name);
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
 
-		if ((*d).age == 0)
-			printf("Age: (nil)\n");
-		else
-			printf("Name: %f\n", (*d).age);
+		printf("Age: %f\n", (d->age) ? d->age : 0);
 
-		if ((*d).owner == NULL)
-			printf("Owner: (nil)\n");
-		else
-			printf("Name: %s\n", (*d).owner);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
