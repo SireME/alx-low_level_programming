c288e4f5b46aeed43c2f93a2253a9504b37ac10d
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-22 13:38:49 +0100

    print name with function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name with function pointers 
+ * presence
+ * @name: name to be printed
+ * @f: pointer function
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
