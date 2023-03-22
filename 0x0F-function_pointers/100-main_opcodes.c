d1b23af723c4ce13e665c8974fcaf66db14f1eb4
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-22 23:15:33 +0100

    print opcode of main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print opcode of main function
+ * @argv: length of finput
+ * argv: input vector
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	int n_bytes;
+
+	n_bytes = 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (n_bytes < atoi(argv[1]))
+	{
+		printf("%02x", *((unsigned char *)main + n_bytes));
+		n_bytes++;
+		if (atoi(argv[1]) > n_bytes)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
