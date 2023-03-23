72e3c3417c18fea249399052cea0c12f18ccd3fa
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-23 13:23:23 +0100

    print delimited strings followed by new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - print strings delimited by any character
+ * @separator: seperator for strings
+ * @n: number of characters
+ *
+ * Return: nothing(print delimited strings)
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+
+{
+	unsigned int i;
+	va_list ap;
+	char *str;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		printf("%s", str == NULL ? "nil" : str);
+
+		if (separator == NULL)
+			continue;
+		else
+		{
+			if (i == n - 1)
+				break;
+			printf("%s", separator);
+		}
+	}
+	va_end(ap);
+	putchar('\n');
+}
