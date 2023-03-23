5219da4be8912bfffeb2898922432c1a5c32ffc8
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-23 21:04:43 +0100

    function to print any input type

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - print all inputs
+ * @format: string of types
+ * @n: number of characters
+ *
+ * Return: nothing(print delimited strings)
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i;
+	char *str;
+	char *delimit;
+	va_list ap;
+
+	va_start(ap, format);
+	i = 0;
+	while (format && format[i])
+	{
+		delimit = "";
+		if (format[i + 1])
+			delimit = ", ";
+		switch (format[i])
+		{
+
+		case 'c':
+			printf("%c%s", va_arg(ap, int), delimit);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(ap, int), delimit);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(ap, double), delimit);
+			break;
+		case 's':
+			str = va_arg(ap, char *);
+			if (!str || !*str)
+				str = "(nil)";
+			printf("%s%s", str, delimit);
+			break;
+		}
+		i++;
+	}
+	putchar('\n');
+}
