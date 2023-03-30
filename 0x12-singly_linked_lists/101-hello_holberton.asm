efddbb1c413269800020b134262e1a14e41d167e
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-30 23:01:41 +0100

    64 bit program to print text followed by new line in assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+  hello db 'Hello, Holberton!', 0Ah, 0
+
+section .text
+  global main
+  extern printf
+
+  main:
+    push rbp
+    mov rbp, rsp
+
+    lea rdi, [hello]
+    mov al, 0
+    call printf
+
+    mov eax, 0
+    pop rbp
+    ret
