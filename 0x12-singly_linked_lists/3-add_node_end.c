a4179814ad21d15bb41e7b1980e90a548a41be27
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-30 22:02:14 +0100

    betty fix: add new node to end of linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,35 +9,34 @@
  *
  * Return: The address of the new element or NULL if it failed.
  **/
-list_t *add_node_end(list_t **head, const char *str) 
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	int len = 0;
-       
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	if (new_node->str == NULL) 
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	while(*str++)/*length calculation*/
+	while (*str++) /*length calculation*/
 		len++;
 	new_node->len = len;
 
 	new_node->next = NULL; /*logic to add new node at end*/
-	if (*head == NULL) {
+	if (*head == NULL)
 		*head = new_node;
-	}
-	else 
+	else
 	{
 		list_t *current = *head;
 
-		while (current->next != NULL) 
+		while (current->next != NULL)
 			current = current->next;
 
 		current->next = new_node;
