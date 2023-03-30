43b53a20b6070b277fafc3c58bf9cde393bb7733
Mbang Ernest N <mbangernest@yahoo.com>
2023-03-30 21:29:36 +0100

    fix: add new note at begining of list_t

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,21 +12,26 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int len = 0;
 
 	if (head == NULL || str == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));/*allocate memory for new var*/
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
+	new_node->str = strdup(str); /*add input str to list_t*/
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
+	while (*str++) /*compute the length of str*/
+		len++;
+
+	new_node->len = len; /*add len variable*/
 	new_node->next = *head;
 	*head = new_node;
 
