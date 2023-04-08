c4d7d09afd3fecce84e2a0bceb12182557c3f401
Mbang Ernest N <mbangernest@yahoo.com>
2023-04-08 13:27:25 +0100

    print binary representation of number, no %, / arrays or malloc

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@
  *header file containing all the function prototypes we are
  *going to use in this project
  */
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
