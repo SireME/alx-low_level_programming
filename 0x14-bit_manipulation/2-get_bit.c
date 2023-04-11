ee30e6859feb26cf7155869377a1ebffd787bff0
Mbang Ernest N <mbangernest@yahoo.com>
2023-04-11 03:49:42 +0100

    fix:control out of range possibility

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,13 +10,8 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int m = n;
-	unsigned int len = 0;
 
 	if (index >= sizeof(n) * 8)
-		return (-1);
-
-	if (index >= len)/*terminate if index is >=len*/
 		return (-1);
 
 	return ((n >> index) & 1); /*return value at index*/
