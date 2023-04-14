15bf548c6a566f0ad920f14cc57b4fa5a60cdb28
Mbang Ernest N <mbangernest@yahoo.com>
2023-04-14 12:50:04 +0100

    fix: copy file1 to file2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,71 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/stat.h>
+
 #define BUFFER_SIZE 1024
+/**
+ * close_and_read - close files and repots errors
+ * @read_count: keep track of bytes read
+ * @file_from: file to read from
+ * @file_to: file to copy from
+ * @firstf: first file name
+ *
+ * Return: nothing
+ */
+void close_and_read(int read_count, int file_from, int file_to, char *firstf)
+{
+	/* Check read error */
+	if (read_count == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", firstf);
+		close(file_from);
+		close(file_to);
+		exit(98);
+	}
+
+	/* Close files */
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		close(file_to);
+		exit(100);
+	}
+
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+}
+/**
+ * main_copy - function to carry out copy operation
+ * @read_count: keep track of bytes read
+ * @ffrom: file to read from
+ * @fto: file to copy to
+ * @buff: buffer
+ * @sf: second file name
+ *
+ * Return: nothing
+ */
+
+void main_copy(int read_count, int ffrom, int fto, char *buff, char *sf)
+{
+	int write_count;
+
+	/* Copy file content */
+	while ((read_count = read(ffrom, buff, BUFFER_SIZE)) > 0)
+	{
+		write_count = write(fto, buff, read_count);
+		if (write_count != read_count)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", sf);
+			close(ffrom);
+			close(fto);
+			exit(98);
+		}
+	}
+}
 
 /**
  * main - copy one file to another
@@ -15,29 +79,26 @@
 
 int main(int argc, char **argv)
 {
-	int file_from, file_to, read_count, write_count, close_status;
+	int file_from, file_to, read_count = 0;
 	char buffer[BUFFER_SIZE];
-	mode_t mode;
+	mode_t mode = 0644;
+	struct stat sts;
 
-	/* Check number of arguments */
-	if (argc != 3)
+	if (argc != 3)/* Check number of arguments */
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
 
-	/* Open source file */
-	file_from = open(argv[1], O_RDONLY);
+	file_from = open(argv[1], O_RDONLY);/* Open source file */
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	/* Check destination file existence and set permissions */
-	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	if (access(argv[2], F_OK) == 0)
-		mode = (mode_t) (mode | S_IWUSR | S_IWGRP | S_IWOTH);
+	if (stat(argv[2], &sts) == 0)/*file permissions*/
+		mode = sts.st_mode & 0777;
 
 	/* Open destination file */
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
@@ -48,42 +109,8 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 
-	/* Copy file content */
-	while ((read_count = read(file_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		write_count = write(file_to, buffer, (ssize_t) read_count);
-		if (write_count != read_count)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(file_from);
-			close(file_to);
-			exit(98);
-		}
-	}
-
-	/* Check read error */
-	if (read_count == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(file_from);
-		close(file_to);
-		exit(98);
-	}
-
-	/* Close files */
-	close_status = close(file_from);
-	if (close_status == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		close(file_to);
-		exit(100);
-	}
-	close_status = close(file_to);
-	if (close_status == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+	main_copy(read_count, file_from, file_to, buffer, argv[2]);
+	close_and_read(read_count, file_from, file_to, argv[1]);
 
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void close_and_read(int read_count, int file_from, int file_to, char *firstf);
+void main_copy(int read_count, int file_from,int file_to,char *buffer,char *secondf);
 
 #endif
