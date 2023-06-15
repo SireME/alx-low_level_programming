6d14a48d27d9a7eb21f0798ba08a967057ed149b
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-15 15:58:33 +0100

    sum content of linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum all linked list content
+ * @head: linked list entry
+ *
+ * Return: sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int list_sum = 0;
+
+	while (head != NULL)
+	{
+		list_sum += head->n;
+		head= head->next;
+	}
+
+	return list_sum;
+}
