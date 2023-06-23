a5d05f5659a256dd9a85e3a89f4cbd8b5d31f145
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-23 21:39:36 +0100

    modify shell hack file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO /tmp/lfinal.so  https://github.com/SireME/alx-low_level_programming/raw/main/0x18-dynamic_libraries/0x18.c/lfinal.so
+wget -qO /tmp/lfinal.so  https://github.com/SireME/alx-low_level_programming/raw/main/0x18-dynamic_libraries/ox18_run/lfinal.so
 export LD_PRELOAD=/tmp/lfinal.so
