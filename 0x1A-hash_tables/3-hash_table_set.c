c8b3b03d8208d6e47a4e900d678e62de58245679
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-07 23:49:40 +0100

    modify key replace functionality

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,52 +9,45 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newnode, *temp;
+	hash_node_t *newnode, *temp, *temp1;
 	int index;
 	char *nvalue;
 	const unsigned char *tempkey = (const unsigned char *) key;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
 	nvalue = strdup(value);
 	if (nvalue == NULL)
 	{
 		free(nvalue);
 		return (0);
 	}
-
 	newnode = create_node(key, nvalue);
 	if (newnode == NULL)
 	{
 		free(nvalue);
 		return (0);
 	}
-
-	/*compute index from size and key using func*/
 	index = key_index(tempkey, ht->size);
-
-	/* add element to hash table */
-	/* update value at computed index if index empty*/
 	if (ht->array[index] == NULL)
 		ht->array[index] = newnode;
-	/*if index not empty, resolve collision by adding to list*/
 	else if (ht->array[index] != NULL)
 	{
 		temp = ht->array[index];
-		while (temp->next != NULL )
+		temp1 = temp;
+		while (temp1 != NULL) /*replace instance*/
 		{
-			if (strcmp(temp->key, key) == 0)
+			if (strcmp(temp1->key, key) == 0)
 			{
-				free(temp->value);
-				strcpy(temp->value, value);
+				free(temp1->value);
+				strcpy(temp1->value, value);
+				return (1);
 			}
-			temp= temp->next;
+			temp1 = temp1->next;
 		}
 		ht->array[index] = newnode;
 		newnode->next = temp;
 	}
-
 	return (1);
 }
 
