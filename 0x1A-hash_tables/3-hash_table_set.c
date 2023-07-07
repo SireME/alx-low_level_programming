e4665d75af0fce384b01c5eacec5e8a1dee3e276
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-07 23:34:14 +0100

    replace already existing key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -42,6 +42,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else if (ht->array[index] != NULL)
 	{
 		temp = ht->array[index];
+		while (temp->next != NULL )
+		{
+			if (strcmp(temp->key, key) == 0)
+			{
+				free(temp->value);
+				strcpy(temp->value, value);
+			}
+			temp= temp->next;
+		}
 		ht->array[index] = newnode;
 		newnode->next = temp;
 	}
