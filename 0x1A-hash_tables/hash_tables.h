1d92ab10393b552916aebc909191ffb4b520bcf0
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-08 02:54:59 +0100

    free all alocated mem

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -58,4 +58,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 
 /* function to print hash table*/
 void hash_table_print(const hash_table_t *ht);
+
+/* delete hash table*/
+void hash_table_delete(hash_table_t *ht);
+
 #endif
