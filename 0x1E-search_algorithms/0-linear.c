dacf529f5f5c109a3ee25f80d8800fe8ce0aa0bf
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-10 13:46:49 +0100

    revamp: linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,12 +17,9 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		if (array[i]  == value)
-		{
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-			return (i);
-		}
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i]  == value)
+			return (i);
 	}
 
 	return (-1);
