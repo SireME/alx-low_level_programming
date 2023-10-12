308e4ff5064ea0da4efedfc3f6b7c64affefda97
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-12 03:47:10 +0100

    interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -34,7 +34,7 @@ int interpolation_search(int *array, size_t size, int value)
 			high = pos - 1;
 	}
 
-	printf("Value checked array[%d] is out of range\n", high);
+	printf("Value checked array[%lu] is out of range\n", pos);
 
 	return (-1);
 }
